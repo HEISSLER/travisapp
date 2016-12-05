d8f1d1c09088c96731d554518dd1215a8a7ed7c1
HEISSLER <h.eissler@de.ibm.com>
2016-12-05 18:15:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,14 +15,29 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+//serve the files out of ./public as our main files
+//app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+
+app.get('/hello', function (req, res) {
+  res.send('hello and good morning');
+});
+
+app.get(express.static(__dirname + '/public'));
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+var server = app.listen(3000, function () {
+  var port = server.address().port;
+ console.log('Example app listening at port %s', port);
+});
+module.exports = server;
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
